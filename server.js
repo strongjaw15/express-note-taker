f1cd7a02a3c5ab144ac74d6afdc15fe85d7611a9
strongjaw15 <119770691+strongjaw15@users.noreply.github.com>
2023-01-31 23:06:41 -0600

    Built the * route, notes route, and get api/notes route. Began work on post api/notes route.

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const fs = require('fs')
 const { v4: uuidv4 } = require('uuid')
 const path = require('path');
+const database = require('./db/db.json')
 
 const app = express(); 
 const PORT = process.env.PORT || 3001;
@@ -12,47 +14,46 @@ app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Route Listeners Go Here
-
 // Home page route
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/')));
 
-// Route with a wildcard 
-app.get("/api/product/:id", (req, res) => {
-  // we access the value of the wildcard via req.params
-  // we might use that wildcard to lookup a database record
-  // we send back whatever is appropriate
+// This is the route for get notes.
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
 })
 
-// This is the route for notes.html
-app.get('/api/notes', (req, res) => {res.sendFile(path.join(__dirname, '/public/notes.html'))})
-
-//  /api/product?sortby=name&sortdir=asc
-app.get("/api/product", (req, res) => {
-  // use req.query to get any possible url params that were submitted
+// This is the route for get api notes.
+app.get('/api/notes', (req, res) => {
+  res.json(database)
 })
 
-app.post('/api/review', (req, res) => {
-  // any data that is submitted will be available in req.body
-
-  res.status(200).json({ result: "success"} )
+// This is the route for post notes.
+app.post('/api/notes', (req, res) => {
+  console.log(req.body)
+  postNotes(req)
+  res.status(200).json(database)
 })
 
-app.put("/api/product/:id", (req, res) => {
-  // any data that is submitted will be available in req.body
-
-  res.status(400).json({ result: "error", msg: "Gimme a real id" })
-})
-
-app.delete("/api/product/:id", (req, res) => {
-  // any data that is submitted will be available in req.body
-})
+// This writes the notes to the database.
+const postNotes = req => {
+  const {title, text} = req.body
+  const newNote = {
+    title,
+    text,
+    noteId: uuidv4()
+  }
+  const notes = JSON.parse(fs.readFile(database))
+  notes.push(newNote)
+  fs.writeFile(database, JSON.stringify(notes))
+  return database
+}
 
 // Catch-all route -- must be last in the list of routes
-app.get("*", (req, res) => res.sendFile(path.join(__dirname, '/public/404.html')))
-
+app.get("*", (req, res) => 
+  res.sendFile(path.join(__dirname, '/public/404.html'))
+)
 
 // Here we tell Express to start listening for request
 app.listen(PORT, () =>
-  console.log(`Serving static asset routes on port ${PORT}!`)
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
